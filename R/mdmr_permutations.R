a0afa103adace8672fe9ece43af0a637a612f05e
Michael Milham <rocky.rfmh.org>
2013-05-08 01:24:31 -0400

    Revert "Revert "added drop argument for laply calls""
    This reverts commit 1eff01c962a0fb77f187cbcb3bc816d49319f75b.

--- R/mdmr_permutations.R
+++ R/mdmr_permutations.R
@@ -233,7 +233,7 @@ mdmr_perms.gather_perms_for_factor <- function(rhs, grps, f.ind, nperms,
             }
         }
         return(ps)
-    }, .progress=progress)
+    }, .progress=progress, .drop=FALSE)
     pmat <- t(pmat)
     
     if (include.orig) 
